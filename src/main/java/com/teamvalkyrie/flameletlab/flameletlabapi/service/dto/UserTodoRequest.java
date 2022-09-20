81bf02ac01889c4ba09bfad8a96a14b08a9e1351
Arekuchan <kasiryeduncan@gmail.com>
2022-09-20 12:11:45 +1000

    Tested todo changes, works. Estimated times need to be in ISO-8601 seconds based representation. https://docs.oracle.com/javase/8/docs/api/java/time/Duration.html#toString--

--- src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/service/dto/UserTodoRequest.java
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/service/dto/UserTodoRequest.java
@@ -3,6 +3,7 @@ package com.teamvalkyrie.flameletlab.flameletlabapi.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.Duration;
 
 @Getter
@@ -11,5 +12,6 @@ public class UserTodoRequest {
     @NotBlank
     private String name;
 
+    @NotNull
     private Duration estimatedTime;
 }
