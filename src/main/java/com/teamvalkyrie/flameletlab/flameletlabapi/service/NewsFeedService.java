e3b379d0ed053485dbb045efcb92029c3f2e9576
Arekuchan <kasiryeduncan@gmail.com>
2022-10-12 16:35:23 +1000

    urmum

--- src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/service/NewsFeedService.java
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/service/NewsFeedService.java
@@ -68,5 +68,4 @@ public class NewsFeedService {
 
         return new ArrayList<>(articles);
     }
-
 }
---
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/service/dto/ArticleResponse.java
@@ -0,0 +1,13 @@
+package com.teamvalkyrie.flameletlab.flameletlabapi.service.dto;
+
+import java.util.List;
+
+public class ArticleResponse {
+    public String name;
+
+    public List<String> tags;
+
+    public String url;
+
+    public String imgUrl;
+}
