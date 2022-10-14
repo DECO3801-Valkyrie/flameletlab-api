d2297dfc38f570cd4f6f9b54a9e74637174137c7
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-14 16:42:41 +1000

    places API

--- src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/controller/GooglePlacesAPIProxyController.java
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/controller/GooglePlacesAPIProxyController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -20,7 +21,7 @@ import org.springframework.web.client.RestTemplate;
  *         { observe: 'response' });
  *   }
  */
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/places-api")
 public class GooglePlacesAPIProxyController {
