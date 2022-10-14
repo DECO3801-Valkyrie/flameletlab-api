968dc1641789833c77d9ca89007264a65f440a02
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-14 20:01:59 +1000

    fix format string issue

--- src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/config/RestTemplateConfiguration.java
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/config/RestTemplateConfiguration.java
@@ -1,7 +1,10 @@
 package com.teamvalkyrie.flameletlab.flameletlabapi.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +12,8 @@ public class RestTemplateConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return  new RestTemplateBuilder()
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
--- src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/controller/GooglePlacesAPIProxyController.java
+++ src/main/java/com/teamvalkyrie/flameletlab/flameletlabapi/controller/GooglePlacesAPIProxyController.java
@@ -1,13 +1,14 @@
 package com.teamvalkyrie.flameletlab.flameletlabapi.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  *
@@ -26,13 +27,24 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/places-api")
 public class GooglePlacesAPIProxyController {
 
-    private static final String GOOGLE_PLACES_API_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCWfP_3D3jCTlXdMvHQa8Y_r0r_iov83Yc&radius=3200000&strictbounds=true&location=-25.274398%%2C133.775136&components=country:au";
+    private static final String GOOGLE_PLACES_API_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCWfP_3D3jCTlXdMvHQa8Y_r0r_iov83Yc&radius=3200000&strictbounds=true&location=-25.274398%2C133.775136&components=country:au";
     private final RestTemplate restTemplate;
 
     @GetMapping("/query")
     public String queryPlaces(@RequestParam("query") String query) {
+        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Content-Type", "application/json; charset=UTF-8");
+//        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36");
         String url  =  GOOGLE_PLACES_API_URL + "&input=" + query;
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        return response.getBody();
+//        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<Object>(headers), String.class);
+
+        WebClient client = WebClient.create();
+
+        WebClient.ResponseSpec responseSpec = client.get()
+                .uri("https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCWfP_3D3jCTlXdMvHQa8Y_r0r_iov83Yc&radius=3200000&strictbounds=true&location=-25.274398%2C133.775136&components=country:au&input=123")
+                .retrieve();
+
+        String responseBody = responseSpec.bodyToMono(String.class).block();
+        return responseBody;
     }
 }
